0c2250b74ee93e39fef4b9001157c19eea64d14a
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-11-01 16:33:53 -0500

    auth setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_193813) do
+ActiveRecord::Schema.define(version: 2020_11_01_211753) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_11_01_193813) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "username"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
